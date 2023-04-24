05ccc232c0a987d7fae24a5a2ca5e6a93ef4053a
esra akbulat <esra@klinikya.com>
2023-04-24 11:45:04 +0300

    -change

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "c914d57c6e3a805f"; }
+/******/ 		__webpack_require__.h = function() { return "3a9a85954051ef85"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
